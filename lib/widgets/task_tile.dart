0bc0dfc531ab7163740e63fffece99d27a8a495a
xwaynebryan <xwaynelil@gmail.com>
2024-10-29 21:57:12 +0300

    Extracted the checkbox widget

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -7,16 +7,33 @@ class TaskTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: const Text(
+    return const ListTile(
+      title: Text(
         'This is a task.',
       ),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {
-          // When checked the task is crossed from the list
-        },
-      ),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+  const TaskCheckbox({
+    super.key,
+  });
+
+  @override
+  State<TaskCheckbox> createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: false,
+      onChanged: (bool? value) {
+        // When checked the task is crossed from the list
+      },
     );
   }
 }
